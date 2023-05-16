0144bbe5ce4f219b1be72d6a1fb6bf5b002b4879
Victor Matheus <victormatheus183@gmail.com>
2023-05-15 21:34:10 -0300

    base modification

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1>Welcome to Make Your Burger</h1>
   </main>
 </template>
